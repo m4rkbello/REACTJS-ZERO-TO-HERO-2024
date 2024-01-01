d23f05e880a3f36a8c0e29a5f65e66467a2093d2
purge710 <johnrussel338@gmail.com>
2024-01-01 12:22:20 +0800

    useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import Employee from './components/Employee';
 
+//useState para ipasa ang data gamit onChange method
+import { useState } from 'react';
+
 
 function App() {
   console.log("DATA");
   const ShowEmployees = true;
+
+  const [role, setRole] = useState('dev');
+
   return (
     <div className="App">
     {console.log('inside return', ShowEmployees)}
     {ShowEmployees ? (
       <div>
+
+      
+      <input type="text" onChange={(e) => {
+        console.log(e.target.value);
+        setRole(e.target.value);
+      }} />
+
       <Employee key="1" name="M4rk" role="tig lung-ag" />
       <Employee key="2" name="MartinGarrix" role="DJ" />
-      <Employee key="3" name="Marshmello" role="" />
+      <Employee key="3" name="Marshmello" role={role} />
 
       </div>
     ):(
