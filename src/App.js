22dfa78311406106205c08a9083e52d7e93d9209
purge710 <johnrussel338@gmail.com>
2024-01-01 11:42:46 +0800

    props drilling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import Employee from './components/Employee';
 
 
 function App() {
+  console.log("DATA");
+  const ShowEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
+    {console.log('inside return', ShowEmployees)}
+    {ShowEmployees ? (
+      <div>
+      <Employee key="1" name="M4rk" role="tig lung-ag" />
+      <Employee key="2" name="MartinGarrix" role="DJ" />
+      <Employee key="3" name="Marshmello" role="" />
 
-        <p>
-          Hello!
-        </p>
-        <Employee />
-      </header>
+      </div>
+    ):(
+      <div>
+        <h1><center>Unauthorized!</center></h1>
+      </div>
+    )}
+
+
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,16 @@
-function Employee(){
-    return <h3>HERE IS AN EMPLOYEE!</h3>
+function Employee(props){
+
+
+    return(
+        <div>
+        <h3>EMPLOYEE {props.name}</h3>
+        {/* FIRST METHOD*/}
+        <p>ROLE {props.role ? props.role :"WALAY TRABAHO"}</p>
+         {/* SECOND METHOD*/}
+        {props.role ? <h1>{props.role}</h1> : <h1>No Role!</h1>}
+        </div>
+
+    ) 
 }
 
 
