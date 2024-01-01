691b3588f050774006e734bcddd4a76fa1581f21
purge710 <johnrussel338@gmail.com>
2024-01-01 15:11:23 +0800

    Map through state Array using object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import Employee from './components/Employee';
-
 //useState para ipasa ang data gamit onChange method
 import { useState } from 'react';
-
+//uuid universal unique identifier id
+import {v4 as uuidv4} from 'uuid';
+ 
 
 function App() {
   console.log("DATA");
   const ShowEmployees = true;
 
   const [role, setRole] = useState('dev');
+
+  const [employees, setEmployees] = useState(
+    [
+      {
+        id: 1,
+        name: 'M4rk Bello',
+        role: 'tig lung-ag',
+        img: 'https://picsum.photos/200/300/?blur'
+      },
+      {
+        id: 2,
+        name: 'Martin Garrix',
+        role: 'DJ',
+        img: 'https://images.pexels.com/photos/2092474/pexels-photo-2092474.jpeg'
+      },
+      {
+        id: 3,
+        name: 'Marshmello',
+        role: 'DJ',
+        img: 'https://images.pexels.com/photos/3064620/pexels-photo-3064620.jpeg'
+      },
+      {
+        id: 4,
+        name: 'Paul Jorge',
+        role: 'MAOY',
+        img: 'https://images.pexels.com/photos/2180882/pexels-photo-2180882.jpeg'
+      },
+      {
+        id: 5,
+        name: 'Kawhi Leonard',
+        role: 'Robot',
+        img: 'https://images.pexels.com/photos/2060951/pexels-photo-2060951.jpeg'
+      },
+    ]
+  );
 
   return (
     <div className="App">
@@ -17,11 +53,13 @@ function App() {
         <div>
 
 
+   
+          <div className="flex flex-wrap justify-center">
           <input type="text" onChange={(e) => {
             console.log(e.target.value);
             setRole(e.target.value);
           }} />
-          <div className="flex flex-wrap justify-center">
+          {/*
             <Employee className="mx-2" key="1" name="M4rk" role="tig lung-ag" />
             <Employee key="2" name="MartinGarrix" role="DJ" />
             <Employee key="3" name="Marshmello" role={role} />
@@ -32,6 +70,24 @@ function App() {
             <Employee className="mx-2" key="1" name="M4rk" role="tig lung-ag" />
             <Employee key="2" name="MartinGarrix" role="DJ" />
             <Employee key="3" name="Marshmello" role={role} />
+          */}
+          
+            {employees.map((employee) => {
+              console.log(employee);
+              console.log("UUIUD DATA", uuidv4());
+              return (
+                <Employee
+                  // key={employee.id} 
+                  key={uuidv4()} 
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img} 
+                />
+              );
+              
+            })}
+       
+       
           </div>
 
         </div>
