7070de930e6c2a10f0ac6a7eda64a7b655d0ccd2
purge710 <johnrussel338@gmail.com>
2024-01-01 13:50:50 +0800

    tailwind css classes

--- src/App.js
+++ src/App.js
@@ -12,26 +12,34 @@ function App() {
 
   return (
     <div className="App">
-    {console.log('inside return', ShowEmployees)}
-    {ShowEmployees ? (
-      <div>
+      {console.log('inside return', ShowEmployees)}
+      {ShowEmployees ? (
+        <div>
 
-      
-      <input type="text" onChange={(e) => {
-        console.log(e.target.value);
-        setRole(e.target.value);
-      }} />
 
-      <Employee key="1" name="M4rk" role="tig lung-ag" />
-      <Employee key="2" name="MartinGarrix" role="DJ" />
-      <Employee key="3" name="Marshmello" role={role} />
+          <input type="text" onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }} />
+          <div className="flex flex-wrap justify-center">
+            <Employee className="mx-2" key="1" name="M4rk" role="tig lung-ag" />
+            <Employee key="2" name="MartinGarrix" role="DJ" />
+            <Employee key="3" name="Marshmello" role={role} />
+            <Employee className="mx-2" key="1" name="M4rk" role="tig lung-ag" />
+            <Employee key="2" name="MartinGarrix" role="DJ" />
+            <Employee key="3" name="Marshmello" role={role} />
+            <Employee key="3" name="Marshmello" role={role} />
+            <Employee className="mx-2" key="1" name="M4rk" role="tig lung-ag" />
+            <Employee key="2" name="MartinGarrix" role="DJ" />
+            <Employee key="3" name="Marshmello" role={role} />
+          </div>
 
-      </div>
-    ):(
-      <div>
-        <h1><center>Unauthorized!</center></h1>
-      </div>
-    )}
+        </div>
+      ) : (
+        <div>
+          <h1><center>Unauthorized!</center></h1>
+        </div>
+      )}
 
 
     </div>
